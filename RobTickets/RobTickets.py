8121a9535d17600bd1af884de04f23df3e6a6a4f
Spcie <m15188159686@163.com>
2018-01-29 23:01:14 +0800

    UPDATA
    -

--- RobTickets/RobTickets.py
+++ RobTickets/RobTickets.py
@@ -9,10 +9,11 @@ top = Tkinter.Tk()
 
 #top.mainloop()
 
+
+
+
+'''
 url_Login = 'https://kyfw.12306.cn/passport/captcha/captcha-check'
-
-
-
 headers = {}
 headers['User-Agent'] = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
 headers['Referer'] = 'https://kyfw.12306.cn/otn/login/init'
@@ -26,7 +27,28 @@ request = urllib2.Request(url_Login, data=data,headers=headers)
 
 respone = urllib2.urlopen(request)
 
+'''
+url_Login ='https://passport.csdn.net/account/verify'
+headers = {}
+headers['User-Agent'] = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+headers['Referer'] = 'https://kyfw.12306.cn/otn/login/init'
+value = {}
+value['gps'] = None
+value['username'] = 'm15188159686@163.com'
+value['password'] = 'jk12345yu'
+value['rememberMe'] = 'true'
+value['lt'] = 'LT-213464-uQeZhb9UG0R5fWa0Bw6sMWu0WO2MWs'
+value['execution'] = 'e3s1'
+value['_eventId'] = 'submit'
 
+data =  urllib.urlencode(value)
+
+#print data
+
+
+request = urllib2.Request(url_Login, data=data,headers=headers)
+
+respone = urllib2.urlopen(request)
 #respone.read()
 #print respone.read()
 f=open('Txt.html','wb')
