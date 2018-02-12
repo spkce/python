e0e80982d3ff57b3b946de9b4d2411465d824e12
Spcie <m15188159686@163.com>
2018-02-12 22:18:00 +0800

    updata

--- WebCrawler/CSDN/CsdnCrawler.py
+++ WebCrawler/CSDN/CsdnCrawler.py
@@ -4,60 +4,52 @@ import urllib2
 import cookielib
 from bs4 import BeautifulSoup
 
-url_login = 'https://passport.csdn.net/account/verify'
-url = "http://blog.csdn.net"
-cookie = cookielib.MozillaCookieJar('cppkies.txt')
-handler=urllib2.HTTPCookieProcessor(cookie)
-opener = urllib2.build_opener(handler)
 
+class CsdnCrawler(object):
+    def __init__(self, cookies_save_path):
+        self.url_login = 'https://passport.csdn.net/account/verify'
+        self.cookie = cookielib.MozillaCookieJar(cookies_save_path)
+        self.handler = handler=urllib2.HTTPCookieProcessor(self.cookie)
+        self.opener = urllib2.build_opener(self.handler)
+    #登陆前准备：获取lt和exection
+    def __get_login_param(self):
+        lt = ''
+        execution = ''
+        self.opener.addheaders = [("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.63 Safari/537.36")]
+        response = self.opener.open(self.url_login)
 
-headers = {}
-headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
-headers['Referer'] = 'https://passport.csdn.net/account/login'
+        soup = BeautifulSoup(response.read(), 'html.parser')
+        lt = soup.find('input', {'name': 'lt'})['value']
+        execution = soup.find('input', {'name': 'execution'})['value']
+        soup.clear()
+        return (lt, execution)
+    #login
+    def login(self, username, password):
+        headers = {}
+        headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+        headers['Referer'] = 'https://passport.csdn.net/account/login'
 
-value = {}
-value['username'] = 'm15188159686@163.com'
-value['password'] = 'Ajk12345yu'
-value['lt'] = 'LT-129493-lYxOUdgttXK3p6SoFZWg94ZPAZU6CW'
-value['execution'] = 'e5s1'
-value['_eventId'] = 'submit'
-
-data = urllib.urlencode(value) 
-
-opener.addheaders = [("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.63 Safari/537.36")]
-#request = urllib2.Request(url_login, data=data, headers)
-
-#登陆前准备：获取lt和exection
-response = opener.open(url_login)
-soup = BeautifulSoup(response.read(),"html5lib")
-for input in  soup.form.find_all("input"):
-    if input.get("name") == "lt":
-        value['lt'] = input.get("value")
-        print value['lt']
-    if input.get("name") == "execution":
-        value['execution'] = input.get("value")
-        print value['execution']
+        lt, execution = self.__get_login_param()
+        value = {}
+        value['username'] = username
+        value['password'] = password
+        value['lt'] = lt
+        value['execution'] = execution
+        value['_eventId'] = 'submit'
+    
+        data = urllib.urlencode(value)
+        response = self.opener.open(self.url_login, data=data)
+        self.cookie.save(ignore_discard=True, ignore_expires=True)
+    #取得登陆后才可见的网页
+    def get_web(self, url):
+        request = urllib2.Request(url)
+        return self.opener.open(request)
  
- 
-response = opener.open(url_login, data=data)
 
-cookie.save(ignore_discard=True, ignore_expires=True)
 
-a={}
 
-for item in cookie:
-    print 'Name = '+item.name
-    print 'Value = '+item.value
-    a[item.name] = item.value
 
-data = urllib.urlencode(a) 
 
-print data
 
-#opener.addheaders[("Cookie",data)]
-request = urllib2.Request('http://msg.csdn.net/letters')
-response = opener.open(request)
 
-f = open('csdn_index.html', 'w')
-f.write(response.read());
-f.close()
+
--- WebCrawler/example.py
+++ WebCrawler/example.py
@@ -1,56 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from bs4 import BeautifulSoup
-import requests
+from CSDN.CsdnCrawler import CsdnCrawler
 
-class CSDN(object):
-    def __init__(self, headers):
-        self.session = requests.Session()
-        self.headers = headers
-    def get_webflow(self):
-        url = 'http://passport.csdn.net/account/login'
-        response = self.session.get(url=url, headers=self.headers)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        lt = soup.find('input', {'name': 'lt'})['value']
-        execution = soup.find('input', {'name': 'execution'})['value']
-        soup.clear()
-        return (lt, execution)
-    def login(self, account, password):
-        self.username = account
-        self.password = password
-        lt, execution = self.get_webflow()
-        data = {
-            'username': account,
-            'password': password,
-            'lt': lt,
-            'execution': execution,
-            '_eventId': 'submit'
-        }
-        url = 'http://passport.csdn.net/account/login'
-        response = self.session.post(url=url, headers=self.headers, data=data)
-        if (response.status_code == 200):
-            print('正常')
-        else:
-            print('异常')
-    def func(self):
-        headers1={
-            'Host':'write.blog.csdn.net',
-            'Upgrade-Insecure-Requests':'1',
-            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
-        }
-        response=self.session.get(url='http://write.blog.csdn.net/postlist',headers=headers1,allow_redirects=False)
-        print response.text
 if __name__ == '__main__':
-    headers = {
-        'Host': 'passport.csdn.net',
-        'Origin': 'http://passport.csdn.net',
-        'Referer':'http://passport.csdn.net/account/login',
-        'Upgrade-Insecure-Requests':'1',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36',
-    }
-    csdn = CSDN(headers=headers)
-    account = ''
-    password = ''
-    csdn.login(account=account, password=password)
-    csdn.func()
+    csdn_Crawler = CsdnCrawler('cookies.txt')
+    csdn_Crawler.login('m15188159686@163.com', 'jk12345yu')
+    response = csdn_Crawler.get_web('http://msg.csdn.net/letters')
+    f = open('csdn_index.html', 'w')
+    f.write(response.read())
+    f.close()
